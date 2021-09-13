67606e64e5b8d33b61e0e2f52fa1d1b730a83ef9
GuilleDB <diazbeltrang@gmail.com>
2021-09-13 18:11:07 -0500

    Finish view data view bag

--- S2_NCORE_MVC.Web/Areas/Marketing/Controllers/HomeController.cs
+++ S2_NCORE_MVC.Web/Areas/Marketing/Controllers/HomeController.cs
@@ -1,17 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using S2_NCORE_MVC.Web.Areas.Marketing.Models;
 
 namespace S2_NCORE_MVC.Web.Areas.Marketing.Controllers
 {
+    [Area("Marketing")]
     public class HomeController : Controller
     {
-        [Area("Marketing")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult ProductsViewModel()
+        {
+            var products = GetProductsJsonLocal();
+            return View(products);
+        }
+        public IActionResult ProductsViewData()
+        {
+            var products = GetProductsJsonLocal();
+            ViewData["ProductList"] = products;
+            return View();
+        }
+        public IActionResult ProductsViewBag()
+        {
+            var products = GetProductsJsonLocal();
+            ViewBag.ProductList= products;
+            return View();
+        }
+        public IEnumerable<Product>GetProductsJsonLocal()
+        {
+            var folder = Path.Combine(Directory.GetCurrentDirectory(), "Areas\\Marketing\\Data\\products.json");
+            var json = System.IO.File.ReadAllText(folder);
+            var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(json);
+            return products;
+
         }
     }
 }
